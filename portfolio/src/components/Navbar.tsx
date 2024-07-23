d415e6266938a0ac641e70caac0a8819490c9a10
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-07-22 21:02:18 -0300

    Add Projects to the Navbar

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -14,6 +14,7 @@ export const Navbar = () => {
     { name: "About", href: "#about" },
     { name: "Experience", href: "#experience" },
     { name: "Skills", href: "#skills" },
+    { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ];
 
