68542f071ee93ef273a24efe4361e603ff282fc8
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-07-30 14:05:17 -0300

    Match the skills section to the new design and color palette

--- portfolio/src/components/Skills.tsx
+++ portfolio/src/components/Skills.tsx
@@ -22,61 +22,50 @@ import {
 } from "react-icons/si";
 
 const skills = [
-  // Programming Languages
   { name: "JavaScript", icon: <SiJavascript /> },
   { name: "TypeScript", icon: <SiTypescript /> },
   { name: "Python", icon: <FaPython /> },
-
-  // Frameworks/Libraries
   { name: "React", icon: <FaReact /> },
   { name: "Next.js", icon: <SiNextdotjs /> },
   { name: "Express.js", icon: <SiExpress /> },
   { name: "Node.js", icon: <FaNode /> },
   { name: "Nest.js", icon: <SiNestjs /> },
-
-  // Styling
   { name: "Tailwind CSS", icon: <SiTailwindcss /> },
-
-  // Tools
   { name: "GitHub", icon: <FaGithub /> },
   { name: "Linux", icon: <FaLinux /> },
   { name: "Bash", icon: <SiGnubash /> },
   { name: "Docker", icon: <FaDocker /> },
-
-  // Databases
   { name: "MySQL", icon: <SiMysql /> },
   { name: "SQLite", icon: <SiSqlite /> },
-
-  // Build Tools
   { name: "Vite", icon: <SiVite /> },
 ];
 
 const skillsClassName =
-  "text-3xl sm:text-4xl lg:text-5xl text-amber-100 transition-transform duration-300";
+  "text-3xl sm:text-4xl lg:text-5xl text-pink-800 transition-transform duration-300";
 
 export const Skills = () => {
   return (
     <section
       id="skills"
-      className="bg-pink-600 text-white p-20 md:p-32 flex flex-col justify-center items-center"
+      className="bg-gradient-to-r from-pink-300 via-pink-500 to-pink-700 text-white p-10 md:p-20 lg:p-32 flex flex-col justify-center items-center"
     >
-      <h4 className="text-amber-100 text-4xl sm:text-5xl lg:text-6xl font-bold mb-8">
+      <h4 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-12">
         Skills
       </h4>
-      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-* gap-8 mt-8">
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-* gap-8">
         {skills.map((skill, index) => (
           <motion.div
             key={index}
-            className="flex flex-col items-center p-4"
+            className="flex flex-col items-center"
             whileHover={{ scale: 1.1, rotate: 10, cursor: "pointer" }}
             whileTap={{ scale: 0.9 }}
           >
             <motion.div
-              className={`bg-gradient-to-r from-amber-300 to-pink-400 p-4 rounded-full ${skillsClassName}`}
+              className={`bg-gradient-to-r from-pink-300 to-pink-100 p-4 rounded-full ${skillsClassName} shadow-lg`}
             >
               {skill.icon}
             </motion.div>
-            <p className="text-amber-100 font-bold mt-2 text-lg sm:text-xl lg:text-2xl">
+            <p className="text-white font-bold mt-4 text-base sm:text-lg lg:text-xl">
               {skill.name}
             </p>
           </motion.div>
