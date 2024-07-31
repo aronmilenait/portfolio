22c54a23b6c64e7b8c9f18f28e025b6bd978ea5e
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-07-31 12:10:04 -0300

    Improve the home's appearence by increasing the image's size and making the section smaller

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -6,9 +6,9 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="bg-gradient-to-r from-pink-300 via-pink-500 to-pink-700 text-white p-10 md:p-20 flex flex-col justify-center items-center min-h-screen"
+      className="bg-gradient-to-r from-pink-300 via-pink-500 to-pink-700 text-white p-10 md:p-20 pt-24"
     >
-      <div className="max-w-screen-lg w-full flex flex-col items-center space-y-10">
+      <div className="flex flex-col items-center space-y-10">
         <motion.div
           className="flex flex-col items-center text-center space-y-6"
           initial={{ opacity: 0, y: -50 }}
@@ -16,7 +16,7 @@ export const Home = () => {
           transition={{ duration: 1, delay: 0.5 }}
         >
           <motion.h1
-            className="text-5xl sm:text-6xl lg:text-7xl font-extrabold text-white mb-4 drop-shadow-lg"
+            className="text-5xl sm:text-6xl lg:text-7xl font-extrabold text-white mb-2 drop-shadow-lg"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 0.5 }}
@@ -39,7 +39,7 @@ export const Home = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1, delay: 1.2 }}
         >
-          <div className="relative w-40 h-40 sm:w-52 sm:h-52 lg:w-64 lg:h-64 rounded-full overflow-hidden border-4 border-white shadow-xl">
+          <div className="relative w-48 h-48 sm:w-64 sm:h-64 lg:w-72 lg:h-72 rounded-full overflow-hidden border-4 border-white shadow-xl">
             <Image src="/myself.jpeg" alt="Photo of Milena Sol Aron" fill />
           </div>
 
