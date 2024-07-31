58bea3baa95e164aee054c86708544a5af9cd145
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-07-31 11:58:49 -0300

    Modify the color palette of the footer

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="bg-pink-800 text-amber-100 flex justify-center items-center p-8">
-      <p>Made by Milena S. Aron using Next.js</p>
+    <footer className="bg-pink-800 text-pink-50 flex justify-center items-center p-8">
+      <p>Made by Milena Sol Aron using Next.js</p>
     </footer>
   );
 };
