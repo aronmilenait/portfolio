3e98a8a6d49750f06cebf4232ac64efd85b1e968
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-10 21:06:44 -0300

    Add new cards to the home component.

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { FaBook, FaPencilAlt, FaMapMarkedAlt } from "react-icons/fa";
+
 export const Home = () => {
   return (
     <section
@@ -40,6 +42,43 @@ export const Home = () => {
         >
           <div className="relative w-48 h-48 sm:w-64 sm:h-64 lg:w-72 lg:h-72 rounded-full overflow-hidden border-4 border-white shadow-xl">
             <Image src="/myself.jpeg" alt="Photo of Milena Sol Aron" fill />
+          </div>
+        </motion.div>
+        <motion.div
+          className="text-xl mt-12 text-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2 }}
+        >
+          <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-lg flex flex-col items-center">
+            <FaMapMarkedAlt className="text-3xl text-white mb-4" />
+            <p className="text-lg text-white font-semibold">
+              Based in Buenos Aires, Argentina. <br></br>
+              My native language is Spanish, and my English proficiency is C2.
+            </p>
+          </div>
+          <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-lg flex flex-col items-center">
+            <FaPencilAlt className="text-3xl text-white mb-4" />
+            <p className="text-lg text-white font-semibold">
+              Developer and founder of{" "}
+              <a
+                href="https://carrerait.vercel.app"
+                className="underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CarreraIT
+              </a>
+              , which intends to to help individuals from Hispanic countries
+              discover their career paths in the tech field.
+            </p>
+          </div>
+          <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-lg flex flex-col items-center">
+            <FaBook className="text-3xl text-white mb-4" />
+            <p className="text-lg text-white font-semibold">
+              Pursuing an Associate&apos;s Degree in Programming at Universidad
+              Tecnológica Nacional.
+            </p>
           </div>
         </motion.div>
       </div>
