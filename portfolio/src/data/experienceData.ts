26049b58c21b48412dfea8c240e3606c948cbecc
Milena S. Aron <134554294+aronmilenait@users.noreply.github.com>
2024-09-10 21:31:52 -0300

    Add CarreraIT, my personal project, in the experience data.

--- portfolio/src/data/experienceData.ts
+++ portfolio/src/data/experienceData.ts
@@ -9,6 +9,19 @@ export interface Experience {
 
 export const workExperience: Experience[] = [
   {
+    company: "CarreraIT",
+    location: "Argentina",
+    role: "Full Stack Developer",
+    timeframe: "September 2024 - Present",
+    responsibilities: [
+      "CarreraIT is a platform I created as a personal project to help individuals from Hispanic countries discover their career paths in the tech field. It offers free, high-quality resources, career orientation guides, and a survey that provides personalized recommendations based on their responses.",
+      "Develop the platform using Next.js, TypeScript, React.js, and Tailwind CSS.",
+      "Write orientation guides in Spanish, my native language.",
+      "Search and incorporate for high-quality, free resources.",
+    ],
+    companyUrl: "https://carrerait.vercel.app",
+  },
+  {
     company: "bitbug",
     location: "Miami, USA (Remote)",
     role: "Full Stack Developer",
