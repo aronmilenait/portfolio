bfa22ced2ce2af4203445afe55c6cf743e2c51f8
Milena Sol Aron <aronmilena.it@gmail.com>
2024-10-24 18:41:07 -0300

    Remove the navbar and footer from the homepage.

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,21 +1,17 @@
-import { Navbar } from "@/components/Navbar";
 import { Home } from "@/components/Home";
 import { Experience } from "@/components/Experience";
 import { Skills } from "@/components/Skills";
 import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
-import { Footer } from "@/components/Footer";
 
 export default function Page() {
   return (
     <main>
-      <Navbar />
       <Home />
       <Experience />
       <Skills />
       <Projects />
       <Contact />
-      <Footer />
     </main>
   );
 }
